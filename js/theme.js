23e70d71c17b3967998c9eab09a775181872cbf0
frozzen-git <frozzen@list.ru>
2025-04-26 18:24:43 +0300

    carousel-control-pamel mouseover

--- js/theme.js
+++ js/theme.js
@@ -1,8 +1,19 @@
 (() => {
     'use strict'
-  
+
     window.addEventListener('DOMContentLoaded', () => {
         document.querySelectorAll('.carousel').forEach((element) => new bootstrap.Carousel(element))
+
+        document.querySelectorAll('.carousel-control-panel').forEach((panel) => {
+            const carousel = bootstrap.Carousel.getInstance(panel.parentElement)
+            for (let element of panel.children) {
+                element.addEventListener('mouseover', () => {
+                    const index = element.getAttribute('data-slide-to')
+                    // console.log(index)
+                    carousel.to(index)
+                })
+            }
+        })
     })
-    })()
+})()
   
